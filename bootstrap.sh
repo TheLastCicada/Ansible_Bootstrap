92af2439db4e2161137e6896478caf8df1e7be40
TheLastCicada <zachary.g.brown@gmail.com>
2015-03-24 08:14:41 -0700

    bootstrap file for prepping a server for Ansible

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+cd ~
+mkdir -p ~/.ssh
+mkdir ansible_key
+cd ansible_key
+wget https://raw.githubusercontent.com/TheLastCicada/Ansible_Public_Key/master/id_rsa.pub
+cd ~
+cat ~/ansible_key/id_rsa.pub >> ~/.ssh/authorized_keys
+chown -R root:root ~/.ssh
+chomd 700 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys

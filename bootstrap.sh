5b7462aa6b15d8409bff2790218ac1ad373cd49a
Zachary Brown <zachary.g.brown@gmail.com>
2015-06-08 16:18:23 -0700

    Add fileserver public key

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,9 +10,13 @@ mkdir -p ~/.ssh
 mkdir ansible_key
 cd ansible_key
 wget https://raw.githubusercontent.com/TheLastCicada/Ansible_Public_Key/master/id_rsa.pub
+cat ./id_rsa.pub >> ~/.ssh/authorized_keys
+rm id_rsa.pub
+wget https://gist.githubusercontent.com/TheLastCicada/b9008fe1c53f7d122af9/raw/327058b3e3fb28c6185e767f73075ce247b89319/id_rsa.pub
+cat ./id_rsa.pub >> ~/.ssh/authorized_keys
 cd ~
-cat ~/ansible_key/id_rsa.pub >> ~/.ssh/authorized_keys
 chown -R root:root ~/.ssh
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/authorized_keys
 rm -rf ~/ansible_key
+
